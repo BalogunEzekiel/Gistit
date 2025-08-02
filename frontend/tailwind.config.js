7cdddfe798d6bc9907ec3f738916fd3ff32dd556
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-08-02 02:17:57 +0100

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// frontend/tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gistitGreen: '#008000',
+        gistitOrange: '#FF6600',
+      },
+    },
+  },
+};
